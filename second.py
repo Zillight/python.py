b69f15a1da19a53e9e293e866dae7d2018e77d8d
Banji Zillight <banjiolawolez@gmail.com>
2023-02-04 18:00:57 +0100

    right and left shift

--- second.py
+++ second.py
@@ -1,5 +1,7 @@
 print(10 & 20) # 01010 & 10100 base 2 = 0
 print(10 | 20) # 01010 | 10100 = 11110 base 2,converting to base 10 = 30
 print(10 ^ 20) # 01010 XOR 10100 = 11110 base 2.
-print(10>>2) # 1010 shift 2 will give 10 base 2 = 2 base 10
+print(10>>2) # 1010 right shift 2 will give 10 base 2 = 2 base 10
 print(20>>2) #10100 shift 2 = 101, to base 10 = 5
+print(5<<1) # 101 left shift 2 = 1010 = 10 base 10
+print(10<<2) # 1010 left shift 2 = 101000 = 40 base 10
