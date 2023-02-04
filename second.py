4f6e44b43519ca25f4ca94b23bf141d68fc226bf
Banji Zillight <banjiolawolez@gmail.com>
2023-02-04 18:42:52 +0100

    if statement cont...

--- second.py
+++ second.py
@@ -1,6 +1,6 @@
-num = 12
-
-if num % 5 == 0 or num % 6 == 0:
-    print("Hello")
-if num % 5 == 0 and num % 6 == 0:
-    print("World")
+num = 10
+if num > 5:
+    num = 20
+    new_num = num * 5
+print(num)
+print(new_num)
