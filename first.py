4f25b1970e3a928a5db43c5ea554b167dc61e2ce
Banji Zillight <banjiolawolez@gmail.com>
2023-02-04 16:56:23 +0100

    counting and indexing

--- first.py
+++ first.py
@@ -1,5 +1,7 @@
 var = "Python"
-x = len(var)
+x = var[0]
 print(x) #spaces are also counted, it counts all the characters including spaces, starting from 1.
 var = "I love Python"
-print(len(var))
+print(var[0])
+print(len(var)-2)
+print(var[len(var)-2])
