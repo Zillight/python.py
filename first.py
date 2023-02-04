c3080fe72529c26f52e95b34bfb56dacdc3f12c7
Banji Zillight <banjiolawolez@gmail.com>
2023-02-04 17:01:30 +0100

    -ve indexing

--- first.py
+++ first.py
@@ -1,7 +1,3 @@
 var = "Python"
-x = var[0]
-print(x) #spaces are also counted, it counts all the characters including spaces, starting from 1.
-var = "I love Python"
-print(var[0])
-print(len(var)-2)
-print(var[len(var)-2])
+x = var[-1]
+print(x) 
