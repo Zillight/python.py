ba743ba41c01a4cd51da89917d0ab00ed2684bda
Banji Zillight <banjiolawolez@gmail.com>
2023-02-04 16:47:34 +0100

    string count

--- first.py
+++ first.py
@@ -1,2 +1,6 @@
+var = "Python"
+x = len(var)
+print(x) #spaces are also counted, it counts all the characters including spaces, starting from 1.
 var = "I love Python"
-print(var) #ensure not to do print("var")
+y = len(var)
+print(y)
