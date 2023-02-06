692fcfce5afb3fdc58810445258548efe840e02b
Banji Zillight <banjiolawolez@gmail.com>
2023-02-06 13:41:00 +0100

    Typecasting to give a floor division

--- first.py
+++ first.py
@@ -2,3 +2,7 @@
 print(28 // 10) # prints only the whole number and ignores the rest
 
 print(28 / 10)
+
+print(int(28 / 10)) # because division will always give float answer, wwe can TYPECAST it by forcing 'int' to make it come out as whole number.
+
+print(int(5/3))
