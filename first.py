e52debc80de1dfcf02df02b3e9e822a4f76dc02a
Banji Zillight <banjiolawolez@gmail.com>
2023-02-06 12:33:33 +0100

    string slicing

--- first.py
+++ first.py
@@ -1,4 +1,11 @@
-#type casting 
+#String slicing
+# Syntax =
+# string [start : end : step]
 
-x = 10 + int("20") # the 'int' converts the variable from being a string to an integer.
-print(x)
+my_string = "This is my string!"
+
+print(my_string[0:4]) # The default for the step is always 1 + Always add 1 to the last number
+print(my_string[0:4:2]) # Step:2 means it will skip every 2 steps
+print(my_string[:]) # Prints everything
+print(my_string[4:]) # cuts out the first 4 letter.
+print(my_string[:10]) # starts from the beginning and ends on the 10th number 
