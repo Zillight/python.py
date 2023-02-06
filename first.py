de4879f48df5fe114e23904cb867a227992b57fd
Banji Zillight <banjiolawolez@gmail.com>
2023-02-06 13:38:13 +0100

    Floor Division

--- first.py
+++ first.py
@@ -1,5 +1,4 @@
-# PEMDAS (BODMAS) Operations
+# floor Division
+print(28 // 10) # prints only the whole number and ignores the rest
 
-print(10 - 3 * 2) #3*2= 6, 10 - 6 = 4. 
-print(3 * 20/5) # 20 / 5 = 4, 3 * 4 = 12, but once there is a divsion in the equation, it prints the answer in a float manner, hence, 12.0
-print(3/20 * 5) # same applies here, answer is 0 .75
+print(28 / 10)
