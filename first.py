c81d9cd6f00eaed24c6714e7f7fe1f861f71c2f6
Banji Zillight <banjiolawolez@gmail.com>
2023-02-06 14:13:45 +0100

    Logical Operators

--- first.py
+++ first.py
@@ -1,11 +1,29 @@
-# Comparison Operators - Boolean Operators
-# >, <, >=, <=, = , !=, == .
+# Logical Operators
+# OR 
+# AND
+'''
+Rules in OR Operator
+True or True = True
+True or False = true
+False or True = True
+False or False = False
+'''
 
-print(2>1)
-print(2==2)
-#Both will print true.
+'''
+Rules for AND Operator
+True and True = True
+True and False = False
+False and False = false
+False and False = False
+'''
 
-x = 2
-y = 5
-y += x
-print(y)
+my_bool = True or False
+print(my_bool) #True
+my_bool = False or False
+print(my_bool) #False
+my_bool = True and False
+print(my_bool) #False
+my_bool = False and False
+print(my_bool) #False
+my_bool = True and True
+print(my_bool) #True
