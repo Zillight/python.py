79e4044185bf17aca9184fe9ab01bc62df460864
Banji Zillight <banjiolawolez@gmail.com>
2023-02-06 13:55:33 +0100

    typecasting the input value

--- first.py
+++ first.py
@@ -3,3 +3,10 @@ y = input("Second number: ")
 
 print(x + y) # When you input x = 10 and y = 20, the answer will be '1020', and that is because it sees everything as strings, like, "10" + "20".
 print(type(x))
+
+a = float(input("Third number: "))
+b = float(input("Fourth number: "))
+
+print(a + b) # When you input x = 10 and y = 20, the answer will be '30.0' and this is because it has been typecasted into float, (you can equally typecast into int) but float gives the user to input any value. 
+
+print(type(a))
