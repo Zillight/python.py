6a377904d4baa68d435d15eb39d6efb8edd7b35d
Banji Zillight <banjiolawolez@gmail.com>
2023-02-06 13:25:55 +0100

    Negative modulus values

--- first.py
+++ first.py
@@ -1,11 +1,4 @@
-x = 5
-y = 2
-z = x/y
-print(z)
-
-# Exponent
-print(5**2) # Five raised to power of two
-
-#Modulus
-print(5 % 2) # remainder 1
-print(4 % 5) # 4 is the dividend, 5 is the divisor, since the divisor is greater than he dividend, the answer is always the dividend 
+# Negative Numbers in Modulus
+print(-28 % 10) # we keep adding 10 to the number until it gives us a +ve number i.e. -28 + 10 = -18 + 10 = -8 + 10 = 2
+print(28 % -10) # Here we keep subtracting -10 from the dividend until we have a -ve value i.e. 28 - 10 = 18 - 10 = 8 - 10 = -2.
+print(-5 % 4) # -5 + 4 = -1 + 4 = 3 
