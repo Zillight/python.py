cbe58c20cd1b93a1844bba14bcdba8ef06568065
Banji Zillight <banjiolawolez@gmail.com>
2023-02-08 00:42:16 +0100

    Fibonacci function

--- third.py
+++ third.py
@@ -1,11 +1,12 @@
-# Recursion
+# Fibonacci Sequence 
 
-def count(num):
-    print(num)
-    #Base case
-    if num == 0:
-        return 0
-        
-    count(num -1)
+# Formula: x(n)=x(n-1)+x(n-2)
 
-count(10)
+def fib(n):
+    if n <= 1:
+        return n
+    else:
+        return fib(n-1) + fib(n-2)
+
+for i in range(10):
+    print(fib(i))
