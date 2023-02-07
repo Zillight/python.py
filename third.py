32678b50641ddb13dca4cb51f912ae934e044989
Banji Zillight <banjiolawolez@gmail.com>
2023-02-07 15:15:10 +0100

    Replace function

--- third.py
+++ third.py
@@ -1,10 +1,5 @@
-# Find Function
-txt = "Hello, there"
-x = txt.find("e") # W can type in 'index' in place of find, just that index will bring out an error when it is not available 
-print(x)
-x = txt.find("e", 5, 10) # 5, 10 >> stating the range where it should find e.
-print(x)
+# Replace Function
+string1 = "Welcome to Programming"
+new_string = string1.replace("Programming", "Python Programming")
 
-x = txt.find("x")
-print(x) # This will print -1 which means invalid
-
+print(new_string)
