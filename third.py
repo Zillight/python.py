952e87864c16c4d8f1b88223c44eee927f2f8488
Banji Zillight <banjiolawolez@gmail.com>
2023-02-07 14:45:32 +0100

    Scope and function

--- third.py
+++ third.py
@@ -1,25 +1,11 @@
-#Function in Python
-def minimum(first, second): #User defined function
-    if first > second:
-        print(second)
-    else:
-        print(first)
+# Scope and Function
 
-minimum(1,5) 
+num = 20
 
-print(min(1,5)) # Inbuilt function
+def multiply(n):
+    n = n * 10 # CAn be rewritten as 'n *= 10'
+    num = n
+    print("The value of num  inside function: ",num)
+multiply(num)
 
-# You can use RETURN instead of PRINT in a User defined function
-
-def mini(first, second): #User defined function
-    if first > second:
-        return(second)
-    else:
-        return(first)
-var = mini(1,5)
-print(var)
-
-# You can use position argument to rearrange the function
-
-var = mini(second=5, first= 1)
-print(var) 
+print(num) # This is a global variable(outside of the function)
