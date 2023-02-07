63389f457baf73d66efe2a9bb93a8cc2e7185559
Banji Zillight <banjiolawolez@gmail.com>
2023-02-07 14:31:50 +0100

    Return == Print

--- third.py
+++ third.py
@@ -8,3 +8,13 @@ def minimum(first, second): #User defined function
 minimum(1,5) 
 
 print(min(1,5)) # Inbuilt function
+
+# You can use RETURN instead of PRINT in a User defined function
+
+def mini(first, second): #User defined function
+    if first > second:
+        return(second)
+    else:
+        return(first)
+var = mini(1,5)
+print(var)
