ab55fcffbe03c5905943ec52658d9714c4c22758
Banji Zillight <banjiolawolez@gmail.com>
2023-02-08 00:15:23 +0100

    Recursion

--- third.py
+++ third.py
@@ -1,15 +1,11 @@
-# Map(), list() and Filter () - Functions 
+# Recursion
 
-# syntax: map(function, list)
-# syntax: filter(function, list)
+def count(num):
+    print(num)
+    #Base case
+    if num == 0:
+        return 0
+        
+    count(num -1)
 
-num_list = [1, 2, 3, 4, 5]
-var = map(lambda n: n*2, num_list)
-print(list(var)) # This list serves as a form of typecasting 
-
-# Result: [2, 4, 6, 8, 10]
-
-var = filter(lambda n: n>2, num_list)
-print(list(var)) 
-
-# Result: [3, 4, 5]
+count(10)
