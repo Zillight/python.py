1cb872710410a113c5dacbcedddb88df386b9aa2
Banji Zillight <banjiolawolez@gmail.com>
2023-02-07 14:45:50 +0100

    changes

--- third.py
+++ third.py
@@ -3,7 +3,7 @@
 num = 20
 
 def multiply(n):
-    n = n * 10 # CAn be rewritten as 'n *= 10'
+    n = n * 10 # Can be rewritten as 'n *= 10'
     num = n
     print("The value of num  inside function: ",num)
 multiply(num)
