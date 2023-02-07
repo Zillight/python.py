44cdd359ccdef378ef0f7bb29e4c46a079bef820
Banji Zillight <banjiolawolez@gmail.com>
2023-02-07 15:12:38 +0100

    Find == Index

--- third.py
+++ third.py
@@ -1,6 +1,6 @@
 # Find Function
 txt = "Hello, there"
-x = txt.find("e")
+x = txt.find("e") # W can type in 'index' in place of find, just that index will bring out an error when it is not available 
 print(x)
 x = txt.find("e", 5, 10) # 5, 10 >> stating the range where it should find e.
 print(x)
