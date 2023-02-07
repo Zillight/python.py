b28a511ad48a447f639b069f0c949c49fbb4747d
Banji Zillight <banjiolawolez@gmail.com>
2023-02-08 00:00:33 +0100

    Function in a function

--- third.py
+++ third.py
@@ -5,5 +5,11 @@ def add(n1, n2):
 def subtract(n1, n2):
     return n1 - n2
 
+def calculate(operation, n1, n2):
+    return operation(n1, n2)
+
 print(add(10, 5))    
-print(subtract(10, 5))    
+print(subtract(10, 5))
+
+print(calculate(add, 10, 5))
+print(calculate(subtract, 10, 5))
