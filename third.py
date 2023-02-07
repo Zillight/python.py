5ceb670e0466c341da462e612b24a2a573375be8
Banji Zillight <banjiolawolez@gmail.com>
2023-02-08 00:10:24 +0100

    Map, list and filter functions

--- third.py
+++ third.py
@@ -1,9 +1,15 @@
 # Map(), list() and Filter () - Functions 
 
 # syntax: map(function, list)
+# syntax: filter(function, list)
 
 num_list = [1, 2, 3, 4, 5]
 var = map(lambda n: n*2, num_list)
-print(list(var)) 
+print(list(var)) # This list serves as a form of typecasting 
 
 # Result: [2, 4, 6, 8, 10]
+
+var = filter(lambda n: n>2, num_list)
+print(list(var)) 
+
+# Result: [3, 4, 5]
