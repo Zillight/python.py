a9c6f13ede17485f7e720937d2777a2eb1a71a16
Banji Zillight <banjiolawolez@gmail.com>
2023-02-07 15:36:34 +0100

    Typecasting

--- third.py
+++ third.py
@@ -1,4 +1,5 @@
-# UNICODE ASCII
+# TYPECASTING
 
-print(ord('a'))
-print(ord('A'))
+#print("Bond" + 7) # THis will throw in an error
+print("Bond" + "007") # Prints Bond007
+print("Bond" + str(7)) # 007 wont work here because of the starting zeroes. 
