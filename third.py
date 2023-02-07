e120c02393b9c7ca1fa9e4bff8376950bd767fac
Banji Zillight <banjiolawolez@gmail.com>
2023-02-07 14:26:54 +0100

    User defined and inbuilt function

--- third.py
+++ third.py
@@ -1,8 +1,10 @@
 #Function in Python
-def minimum(first, second):
+def minimum(first, second): #User defined function
     if first > second:
         print(second)
     else:
         print(first)
 
-minimum(1,5)
+minimum(1,5) 
+
+print(min(1,5)) # Inbuilt function
